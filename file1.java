d273b544cbe49fca98750a7dcdb01bc5ec7a080e
nitinpabreja <nitin.pabreja@gmail.com>
2022-01-29 16:46:00 +0000

    my first commit of source code

---
+++ file1.java
@@ -0,0 +1,9 @@
+total 3248
+-rw-rw-r-- 1 nitinpabrejagma nitinpabrejagma       5 Jan 29 16:30 check_idle
+drwxr-xr-x 2 jenkins         jenkins            4096 Jan 29 15:29 hsperfdata_jenkins
+drwxr-xr-x 2 root            root               4096 Jan 29 15:30 hsperfdata_root
+drwx------ 2 jenkins         jenkins            4096 Jan 29 15:29 jetty-0_0_0_0-8080-war-_-any-5088469214605534884
+drwx------ 2 nitinpabrejagma nitinpabrejagma    4096 Jan 29 15:31 ssh-oxznRW1HOGSz
+drwx------ 3 root            root               4096 Jan 29 15:29 systemd-private-62b069dd598a4f31b027124202bf1b72-rtkit-daemon.service-SY8kHL
+drwx------ 3 root            root               4096 Jan 29 15:29 systemd-private-62b069dd598a4f31b027124202bf1b72-systemd-timesyncd.service-WKNxKW
+-rw-r--r-- 1 jenkins         jenkins         3293397 Jan 29 15:29 winstone8076513927181611655.jar
---
+++ file2.java
@@ -0,0 +1,8 @@
+check_idle
+hsperfdata_jenkins
+hsperfdata_root
+jetty-0_0_0_0-8080-war-_-any-5088469214605534884
+ssh-oxznRW1HOGSz
+systemd-private-62b069dd598a4f31b027124202bf1b72-rtkit-daemon.service-SY8kHL
+systemd-private-62b069dd598a4f31b027124202bf1b72-systemd-timesyncd.service-WKNxKW
+winstone8076513927181611655.jar
